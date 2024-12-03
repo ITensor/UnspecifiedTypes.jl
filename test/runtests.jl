c158282d82ee1ccde26da770ec00c513e1f434c9
Lukas Devos <ldevos98@gmail.com>
2024-12-03 11:40:09 -0500

    formatter (to retrigger actions (again))

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@
 using Test: @testset
 
 @testset "UnspecifiedTypes.jl" begin
-    filenames = filter(readdir(@__DIR__)) do f
-        startswith("test_")(f) && endswith(".jl")(f)
-    end
-    @testset "Test $filename" for filename in filenames
-        include(filename)
-    end
+  filenames = filter(readdir(@__DIR__)) do f
+    startswith("test_")(f) && endswith(".jl")(f)
+  end
+  @testset "Test $filename" for filename in filenames
+    include(filename)
+  end
 end
 
 end
