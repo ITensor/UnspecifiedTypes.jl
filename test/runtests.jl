1ddb88d8b29e9cadebefbc69247bddaef3ee00d2
Lukas Devos <ldevos98@gmail.com>
2024-12-09 18:44:21 -0500

    update with skeleton v0.2.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using SafeTestsets: @safetestset
-using Suppressor: @suppress
+using Suppressor: Suppressor
 
 # check for filtered groups
 # either via `--group=ALL` or through ENV["GROUP"]
@@ -13,11 +13,12 @@ const GROUP = uppercase(
   end,
 )
 
-function istestfile(filename)
-  return isfile(filename) &&
-         endswith(filename, ".jl") &&
-         startswith(basename(filename), "test")
-end
+"match files of the form `test_*.jl`, but exclude `*setup*.jl`"
+istestfile(fn) =
+  endswith(fn, ".jl") && startswith(basename(fn), "test_") && !contains(fn, "setup")
+"match files of the form `*.jl`, but exclude `*_notest.jl` and `*setup*.jl`"
+isexamplefile(fn) =
+  endswith(fn, ".jl") && !endswith(fn, "_notest.jl") && !contains(fn, "setup")
 
 @time begin
   # tests in groups based on folder structure
@@ -33,7 +34,7 @@ end
 
   # single files in top folder
   for file in filter(istestfile, readdir(@__DIR__))
-    (file == basename(@__FILE__)) && continue
+    (file == basename(@__FILE__)) && continue # exclude this file to avoid infinite recursion
     @eval @safetestset $file begin
       include($file)
     end
@@ -41,9 +42,20 @@ end
 
   # test examples
   examplepath = joinpath(@__DIR__, "..", "examples")
-  for file in filter(endswith(".jl"), readdir(examplepath; join=true))
-    @suppress @eval @safetestset $file begin
-      include($file)
+  for (root, _, files) in walkdir(examplepath)
+    contains(chopprefix(root, @__DIR__), "setup") && continue
+    for file in filter(isexamplefile, files)
+      filename = joinpath(root, file)
+      @eval begin
+        @safetestset $file begin
+          $(Expr(
+            :macrocall,
+            GlobalRef(Suppressor, Symbol("@suppress")),
+            LineNumberNode(@__LINE__, @__FILE__),
+            :(include($filename)),
+          ))
+        end
+      end
     end
   end
 end
