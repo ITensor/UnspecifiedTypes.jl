cb0a069712f4bcf60d2729580b9e98da6949a20d
mtfishman <mfishman@flatironinstitute.org>
2025-01-16 09:13:57 -0500

    Format

--- test/test_basics.jl
+++ test/test_basics.jl
@@ -4,21 +4,13 @@ using Test: @testset, @test
 @testset "UnspecifiedTypes (eltype=$elt)" for elt in (
   Float32, Float64, Complex{Float32}, Complex{Float64}
 )
-
-  for x in (
-    elt(2) + UnspecifiedZero(),
-    UnspecifiedZero() + elt(2),
-  )
+  for x in (elt(2) + UnspecifiedZero(), UnspecifiedZero() + elt(2))
     @test x isa elt
     @test x === elt(2)
   end
 
-  for x in (
-    elt(2) * UnspecifiedZero(),
-    UnspecifiedZero() * elt(2),
-  )
+  for x in (elt(2) * UnspecifiedZero(), UnspecifiedZero() * elt(2))
     @test x isa UnspecifiedZero
     @test x === UnspecifiedZero()
   end
-
 end
